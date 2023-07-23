11b737688698ed9f17f633cbf7e6904f34fa2c9b
Krutarth Zala <krutarthzala2@gmail.com>
2023-07-23 11:38:13 +0530

    Correct first program

--- Pointer Programs/P1.c
+++ Pointer Programs/P1.c
@@ -5,6 +5,6 @@ void main()
     int a = 10,b = 20, *p1 = &a, *p2 = &b;
     p1 = p2;
 
-    printf("p1 = ",p1); 
-    printf("p2 = ",p2); 
+    printf("p1 = %d",*p1); 
+    printf("p2 = %d",*p2); 
 }
